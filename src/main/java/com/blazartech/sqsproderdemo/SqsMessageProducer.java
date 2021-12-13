1917acee3d80f3523a2fab87b20495da7d270ce5
aar1069 <scottaaron@northwesternmutual.com>
2021-12-13 09:16:27 -0600

    version update

--- src/main/java/com/blazartech/sqsproderdemo/SqsMessageProducer.java
+++ src/main/java/com/blazartech/sqsproderdemo/SqsMessageProducer.java
@@ -1,41 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.blazartech.sqsproderdemo;
 
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.stereotype.Component;
 
 /**
  *
  * @author aar1069
  */
-@Component
-@Slf4j
-public class SqsMessageProducer {
-    
-    private final QueueMessagingTemplate queueMessagingTemplate;
-    
-    @Value("${demo.queueName}")
-    private String queueName;
+public interface SqsMessageProducer {
 
-    public SqsMessageProducer(QueueMessagingTemplate queueMessagingTemplate) {
-        this.queueMessagingTemplate = queueMessagingTemplate;
-    }
+    <T> void send(T message, Map<String, Object> headers);
     
-    public <T> void send(T message, Map<String, Object> headers) {
-        if (message == null) {
-            log.warn("SQS producer cannot produce null message");
-            return;
-        }
-        
-        log.info("Message: {}" + message);
-        log.info("Queue name {} " + queueName);
-        
-        queueMessagingTemplate.convertAndSend(queueName, message, headers);
-    }
 }
